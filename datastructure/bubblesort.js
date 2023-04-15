bd610a84a03571702efac2b0846b5b9f1337737a
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-04-15 18:42:01 +0530

    sorting programs added

---
+++ datastructure/bubblesort.js
@@ -0,0 +1,18 @@
+// bubble sort
+function bubblesort(array){
+    let n = array.length
+for(let i=n;i>0;i--){
+    let swapped  // to check whether array is sorted or not
+    for(let j=0;j<i-1;j++){
+        if(array[j] > array[j+1]){
+            [array[j],array[j+1]] = [array[j+1],array[j]]
+            swapped = true
+        }
+    }
+    if(!swapped)
+    break
+}
+return array
+}
+console.log(bubblesort([99,1,2,3,5,6]));
+// console.log(bubblesort([2,3,2,1,1,5,8,1]));
