95382b68fdb0f437732980c0b6e335530bb6df5a
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-04-17 14:50:33 +0530

    quick sort added

---
+++ sort/quicksort.js
@@ -0,0 +1,19 @@
+// Quick Sort
+function quicksort(arr) {
+    if(arr.length<=1){
+        return arr
+    }
+    let pivot = arr[arr.length-1]
+    let left = []
+    let right = []
+  for(let i = 0;i<arr.length-1;i++){
+    if(arr[i]<pivot){
+        left.push(arr[i])
+    }
+    else{
+        right.push(arr[i])
+    }
+  }
+  return [...quicksort(left),pivot,...quicksort(right)]
+}
+console.log(quicksort([4,6,-2,15,9,0]));
