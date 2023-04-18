940ad36f1350213c4903a0345edc5bb5242a8460
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-04-18 17:01:30 +0530

    fibinocci series

--- fibinocci.js
+++ fibinocci.js
@@ -1,11 +1,16 @@
 // Fibinocci series
 function fibinocci(n){
-let fib = [0,1]
+  
+let fib =[0,1]
+if(n<1)
+return null
+if(n == 1)
+return 0
 for(let i=2;i<n;i++){
     fib[i] = fib[i-1]+fib[i-2]
 }
 return fib
 
 }
-console.log(fibinocci(2));
+console.log(fibinocci(0));
 console.log(fibinocci(7));
