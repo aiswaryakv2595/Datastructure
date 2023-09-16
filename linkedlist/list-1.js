bc999520b7a9071e1e67d24b53deb27cd701ba10
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-09-16 11:15:43 +0530

    programs added

--- linkedlist/list-1.js
+++ linkedlist/list-1.js
@@ -39,12 +39,56 @@ class LinkedList{
         }
         this.head = prev
     }
+    addList(list1,list2){
+        let current1 = list1.head
+        let current2 = list2.head
+        let value1
+        let value2
+        
+        while(current1){
+           value1 = current1.value
+            current1 = current1.next
+        }
+        while(current2){
+            value2 = current2.value
+            current2 = current2.next
+        }
+   let sum = value1+value2
+   
+        return sum
+    }
+    createLinkedListFromNumber(number) {
+        const numStr = number.toString();
+        const reversedNumStr = numStr.split('').reverse().join('');
+        const resultLinkedList = new LinkedList();
+    
+        for (let i = 0; i < reversedNumStr.length; i++) {
+          const digit = parseInt(reversedNumStr[i]);
+          resultLinkedList.insertFirst(digit);
+        }
+    
+        return resultLinkedList;
+      }
 }
 const list = new LinkedList()
-list.insertFirst(10)
-list.insertFirst(20)
-list.insertFirst(30)
+list.insertFirst(22)
+list.insertFirst(3)
+list.insertFirst(1)
 list.print()
 console.log("-------------")
-list.reverse()
+// list.reverse()
 list.print()
+// 1,3,22
+// 5,20,23
+console.log("-------------")
+const list2 = new LinkedList()
+list2.insertFirst(23)
+list2.insertFirst(20)
+list2.insertFirst(5)
+list2.print()
+
+
+const sum = list.addList(list, list2);
+const reversedSumList = list.createLinkedListFromNumber(sum);
+console.log('Reversed Sum List:');
+console.log(reversedSumList)
